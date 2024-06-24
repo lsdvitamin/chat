8c83347b20b67a7e44c20194b4c3acd99d3a1abc
lsdvitamin <lsdvitamin7@gmail.com>
2024-06-24 19:50:22 +0300

    Исходный код лекции 22

---
+++ client/src/main/java/ru/otus/june/chat/Main.java
@@ -0,0 +1,11 @@
+package ru.otus.june.chat;
+
+/**
+ * @author Sergei on 24.06.2024 19:11.
+ * @progect Default (Template) Project
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
